f60c6dd27a551b671d6fafde2f4fbee360e76efa
vigneshwaran-ramaswamy-snowflake <123058352+vigneshwaran-ramaswamy-snowflake@users.noreply.github.com>
2023-01-19 13:25:28 +0530

    Update streamlit_app.py
    Typo error fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-stremlit.text('Omega 3 & Blueberry Oatmeal')
-stremlit.text('Kale, Spinach & Rocket Smoothie')
-stremlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
